990bedde3e15611efc586232d9ba7a5d23aa2acf
Diego Millan <diego1millan@gmail.com>
2024-01-24 15:58:17 -0600

    updated to port/host settings to serve heroku on the server.py file

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask_app.controllers import users
 from flask_app.controllers import cars
 from flask_app import app
+import os
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 # def create_app():
 #     if __name__ == "__main__":
